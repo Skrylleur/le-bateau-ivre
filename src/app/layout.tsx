10e604b4bf95df1833b7935f94545ca32e5b8d71
Skrylleur <106743803+Skrylleur@users.noreply.github.com>
2025-07-27 16:58:29 +0200

    Changement barre de statut téléphone

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -91,7 +91,8 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <meta name="theme-color" content="#1e40af" />
+        <meta name="theme-color" content="#f59e0b" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         
         {/* Schema.org Restaurant */}
